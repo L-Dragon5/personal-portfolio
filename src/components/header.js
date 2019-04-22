c385959554b12e25319e498a74a64595c91b39c4
Joseph Oh <me@joseph-oh.com>
2019-04-22 18:37:19 -0400

    Updated navigation with hamburger style menu. Absolutely centered intro heading. Gave header fixed height.

--- src/components/header.js
+++ src/components/header.js
@@ -5,15 +5,35 @@ const OuterLink = (props) => (
   <a href={ props.to } className="navbar__link" target="_blank" rel="noopener noreferrer">{ props.children }</a>
 )
 
+class NavbarIcon extends React.Component {
+  onClick = (e) => {
+    e.preventDefault();
+    let navbar = document.getElementsByClassName("navbar")[0];
+    if(navbar.classList.contains("navbar--open")) {
+      navbar.classList.remove("navbar--open");
+    } else {
+      navbar.classList.add("navbar--open");
+    }
+  }
+
+  render() {
+    return(
+      <a href className="navbar__icon" onClick={ this.onClick }><span className="hamburger"></span></a>
+    )
+  }
+}
+
 const Header = () => (
   <header>
     <div className="navbar">
-      <Link to="about" offset={-64} spy={true} smooth={true} duration={400} activeClass="navbar__link--active" className="navbar__link">About</Link>
-      <Link to="work" offset={-64} spy={true} smooth={true} duration={400} activeClass="navbar__link--active" className="navbar__link">Work</Link>
-      <Link to="contact" offset={-64} spy={true} smooth={true} duration={400} activeClass="navbar__link--active" className="navbar__link">Contact</Link>
+      <Link to="about" offset={-62} spy={true} smooth={true} duration={400} activeClass="navbar__link--active" className="navbar__link">About</Link>
+      <Link to="work" offset={-62} spy={true} smooth={true} duration={400} activeClass="navbar__link--active" className="navbar__link">Work</Link>
+      <Link to="contact" offset={-62} spy={true} smooth={true} duration={400} activeClass="navbar__link--active" className="navbar__link">Contact</Link>
       <OuterLink to="https://docs.google.com/document/d/1JiHwoCyBUoEWvgm1F1g0zXwt_TSVxhCP1L4yKUVuyAQ/pub">Resume</OuterLink>
       <OuterLink to="https://github.com/L-Dragon5">GitHub</OuterLink>
     </div>
+
+    <NavbarIcon />
   </header>
 )
 
